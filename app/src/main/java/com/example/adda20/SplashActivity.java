32281cee0ed1075087ff306b554b6ba1231ed4f7
Roy025 <53114983+Roy025@users.noreply.github.com>
2021-07-17 00:05:39 +0600

    life is pain

---
+++ app/src/main/java/com/example/adda20/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.adda20;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+//Intent diye ak activity the next activity te jay, ekhane splash theke main activity te jache
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
